0dc4baac217d17e2ed92ec5730732f6e9e98da87
Dingus-boi <160445793+Dingus-boi@users.noreply.github.com>
2024-02-19 02:09:41 +0000

    Add files via upload
    First upload.

---
+++ P1LAB1_EvansNicholas.py
@@ -0,0 +1,12 @@
+#Nicholas Evans
+#Feb 18 2024
+#P1LAB1
+#I used ubuntu 22.04's generic text editor instead of an IDE.
+
+
+#!/usr/bin/env python3
+
+print('Enter your name here:', end=' ')
+ur_name = input ()
+print('Hey', ur_name,'\nWelcome to Zybooks!')
+
