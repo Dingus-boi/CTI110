038e3b9d6e44fc269a6d35480c5f97d5c03ac9de
Dingus-boi <160445793+Dingus-boi@users.noreply.github.com>
2024-05-07 14:07:01 +0000

    Add files via upload
    A calculator for different mathematical functions.

---
+++ P1HW1_EvansNicholas.py
@@ -0,0 +1,16 @@
+#Nicholas Evans
+#2/27/2014
+#P1HW1
+#A calculator for different mathematical functions
+
+#!/usr/bin/env python3
+
+print('Enter integer:')
+int_1 = int(input())
+print('You entered: ', int_1)
+print(int_1, 'squared is', int_1 * int_1)
+print('And', int_1, 'cubed is', int_1 * int_1 * int_1, '!!')
+print('Enter another integer:')
+int_2 = int(input())
+print(int_1, '+', int_2, 'is', int_1 + int_2)
+print(int_1, '*', int_2, 'is', int_1 * int_2)
